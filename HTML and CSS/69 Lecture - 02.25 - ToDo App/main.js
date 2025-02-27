b61b541e858dbe2a73723bc0e8f2c6ba4dbfa504
BanRokas <rokas.banaitis@gmail.com>
2025-02-27 20:47:53 +0200

    70 Lecture - ToDoApp display tasks from localStorage

--- HTML and CSS/69 Lecture - 02.25 - ToDo App/main.js
+++ HTML and CSS/69 Lecture - 02.25 - ToDo App/main.js
@@ -1,24 +1,3 @@
-document
-  .querySelector('#addNewTask')
-  .addEventListener('submit', e => {
-    e.preventDefault();
-
-    const inputValue = e.target.elements.task.value;
-    const task = {
-      id: Number(new Date()).toString(),
-      name: inputValue,
-      isCompleted: false
-    };
-    const taskDiv = new ToDoTask(task);
-
-    const localStorageData = JSON.parse(localStorage.getItem('toDoData')) || [];
-    localStorageData.push(task);
-    localStorage.setItem('toDoData', JSON.stringify(localStorageData));
-
-    document.querySelector('#tasks').appendChild(taskDiv);
-    e.target.reset();
-  });
-
 class ToDoTask{
   constructor({ id, name, isCompleted }){
     this.id = id;
@@ -46,3 +25,30 @@ class ToDoTask{
     return taskDiv;
   }
 }
+
+const localStorageTasks = JSON.parse(localStorage.getItem('toDoData'));
+localStorageTasks.forEach(task => {
+  const taskDiv = new ToDoTask(task);
+  document.querySelector('#tasks').appendChild(taskDiv);
+});
+
+document
+  .querySelector('#addNewTask')
+  .addEventListener('submit', e => {
+    e.preventDefault();
+
+    const inputValue = e.target.elements.task.value;
+    const task = {
+      id: Number(new Date()).toString(),
+      name: inputValue,
+      isCompleted: false
+    };
+    const taskDiv = new ToDoTask(task);
+
+    const localStorageData = JSON.parse(localStorage.getItem('toDoData')) || [];
+    localStorageData.push(task);
+    localStorage.setItem('toDoData', JSON.stringify(localStorageData));
+
+    document.querySelector('#tasks').appendChild(taskDiv);
+    e.target.reset();
+  });
