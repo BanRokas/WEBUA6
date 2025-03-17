aa8374b3e78bf8b8f1815bbd7cdd123b9e3fc180
BanRokas <rokas.banaitis@gmail.com>
2025-03-17 18:54:55 +0200

    76 Lecture - Tours section with JS generation

--- HTML and CSS/74-76 Lecture - 03.10-03.17 - Practice/main.js
+++ HTML and CSS/74-76 Lecture - 03.10-03.17 - Practice/main.js
@@ -13,6 +13,12 @@
       });
 
       // code for displaying tours cards
+      const toursContainer = document.querySelector('#tours > .toursContainer');
+      data.tours.forEach(tour => {
+        const tourCard = createTourCard(tour);
+        // console.log(tourCard);
+        toursContainer.appendChild(tourCard);
+      });
     });
 })();
 
@@ -30,4 +36,61 @@ function createServiceCard(card){
 
   cardDiv.append(icon, heading, par);
   return cardDiv;
+}
+
+function createTourCard(card){
+  const tourCard = document.createElement('div');
+
+  // div.image
+  const imageDiv = document.createElement('div');
+  imageDiv.classList.add('image');
+
+  const image = document.createElement('img');
+  image.setAttribute('src', card.image);
+  image.setAttribute('alt', card.title);
+
+  const par = document.createElement('p');
+  par.textContent = card.date;
+
+  imageDiv.append(image, par);
+
+  // div.info
+  const infoDiv = document.createElement('div');
+  infoDiv.classList.add('info');
+
+  const heading = document.createElement('h3');
+  heading.textContent = card.title;
+
+  const infoChildDiv = document.createElement('div');
+
+  const mapLink = document.createElement('a');
+  mapLink.setAttribute('href', card.mapsLink);
+  mapLink.setAttribute('target', '_blank');
+
+  const mapIcon = document.createElement('i');
+  mapIcon.classList.add('bi', 'bi-map-fill');
+
+  const locationName = document.createElement('span');
+  locationName.textContent = card.location;
+
+  mapLink.append(mapIcon, locationName);
+
+  const infoChildChildDiv = document.createElement('div');
+
+  const tourDuration = document.createElement('span');
+  // tourDuration.textContent = card.duration + ' Days';
+  tourDuration.textContent = `${card.duration} Days`;
+
+  const tourPrice = document.createElement('span');
+  tourPrice.textContent = `From $${card.price}`;
+
+  infoChildChildDiv.append(tourDuration, tourPrice);
+
+  infoChildDiv.append(mapLink, infoChildChildDiv);
+
+  infoDiv.append(heading, infoChildDiv);
+
+  tourCard.append(imageDiv, infoDiv);
+
+  return tourCard;
 }
